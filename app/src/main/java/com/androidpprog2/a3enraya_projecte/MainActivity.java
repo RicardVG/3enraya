8169cf83fef29c8c47b0c4e46bbc788451b634b2
RicardVG <ricardvinolas10@gmail.com>
2022-02-26 18:09:19 +0100

    Disseny Layout de l'aplicacio (Botons, torns i guanyador)

--- app/src/main/java/com/androidpprog2/a3enraya_projecte/MainActivity.java
+++ app/src/main/java/com/androidpprog2/a3enraya_projecte/MainActivity.java
@@ -3,12 +3,19 @@ package com.androidpprog2.a3enraya_projecte;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textVictory;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textVictory = (TextView) findViewById(R.id.textVictory);
+        textVictory.setVisibility(View.INVISIBLE);
     }
 }
