f55a5d8a7f0a7e772e7fa0d4af220b12945b1929
RicardVG <ricardvinolas10@gmail.com>
2022-02-27 14:29:58 +0100

    Funciona perfi els torns, posa primer una X i despres un cercle correctament! :)

--- app/src/main/java/com/androidpprog2/a3enraya_projecte/MainActivity.java
+++ app/src/main/java/com/androidpprog2/a3enraya_projecte/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Arrays;
 import java.util.Random;
@@ -16,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
     TextView textPlayer1;
     TextView textPlayer2;
     Integer[] buttons;
+    int player = 1;
     int[] table = new int[]{
             0, 0, 0,
             0, 0, 0,
@@ -38,31 +40,41 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void addToken(View v){
-        int size = 10;
-
+        boolean shoot = false;
         int numButton = Arrays.asList(buttons).indexOf(v.getId());
+
         textPlayer1 = (TextView) findViewById(R.id.t1);
-        textPlayer1.setVisibility(View.VISIBLE);
-
         textPlayer2 = (TextView) findViewById(R.id.t2);
-        textPlayer2.setVisibility(View.INVISIBLE);
+        textPlayer1.setVisibility(View.INVISIBLE);
+        textPlayer2.setVisibility(View.VISIBLE);
         textVictory.setVisibility(View. INVISIBLE);
-        table[numButton] = 1;
 
-        for (int i = 0; i < size; i++){
-            v.setBackgroundResource(R.drawable.circulo_negro);
-            textPlayer1.setVisibility(View.INVISIBLE);
-            textPlayer2.setVisibility(View.VISIBLE);
-            Player2(numButton);
-
+        if(table[numButton] == 0 && player == 1){
+            v.setBackgroundResource(R.drawable.circulo_verde);
+            table[numButton] = 1;
+            shoot = true;
         }
+
+        if(table[numButton] == 0 && player == 2){
+            v.setBackgroundResource(R.drawable.cruz_verde);
+            textPlayer1.setVisibility(View.VISIBLE);
+            textPlayer2.setVisibility(View.INVISIBLE);
+            table[numButton] = 2;
+            shoot = true;
+        }
+
+        if (shoot){
+            if(player==1){
+                player=2;
+            }else{
+                player=1;
+            }
+        }
+/*
+        if (table[numButton] == 0){
+            Toast.makeText(MainActivity.this,"No se puede seleccionar esta casilla ocupada!",Toast.LENGTH_SHORT).show();
+        }
+  */
     }
 
-    public void Player2(int numButton) {
-        Button b = (Button) findViewById(buttons[numButton]);
-        b.setBackgroundResource(R.drawable.cruz_negra);
-        table[numButton] = -1;
-        textPlayer1.setVisibility(View.VISIBLE);
-        textPlayer2.setVisibility(View.INVISIBLE);
-    }
 }
